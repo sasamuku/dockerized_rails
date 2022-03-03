e8cb454ac9211e6ec55dfd8d2504f1dff05d8529
sasamuku <sasazawa3330@gmail.com>
2022-03-03 19:19:32 +0900

    add env

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,7 +2,7 @@ Datadog.configure do |c|
     # This will activate auto-instrumentation for Rails
     c.use :rails, service_name: "datadog-tracing-test"
     c.tracer.enabled = true
-    c.tracer.hostname = 'datadog'
+    c.tracer.hostname = ENV['DD_AGENT_HOST']
     c.tracer.port = 8126
     c.diagnostics.debug = true
   end
